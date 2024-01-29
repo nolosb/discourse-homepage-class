b6317c6e509fbfe7a908e4bfce59d0ec15200c17
Manuel <git@kostka.studio>
2024-01-29 11:55:38 +0000

    FEAT: Add top menu setting

--- javascripts/discourse/initializers/homepage-class.js
+++ javascripts/discourse/initializers/homepage-class.js
@@ -2,14 +2,31 @@ import { withPluginApi } from 'discourse/lib/plugin-api';
 
 export default {
   name: 'homepage-class-init',
-  initialize() {
+  initialize(container) {
+    this.site = container.lookup('service:site');
+    this.siteSettings = container.lookup('service:site-settings');
+
     withPluginApi('0.8.24', (api) => {
       api.onPageChange(() => {
         const router = api.container.lookup('service:router');
-        if (router.currentRoute.name === `${settings.homepage}`) {
-          document.body.classList.add('homepage');
-        } else {
-          document.body.classList.remove('homepage');
+        const currentRoute = router.currentRoute.name;
+        switch (settings.homepage) {
+          case 'discovery.latest':
+          case 'discovery.top':
+          case 'discovery.categories':
+            if (router.currentRoute.name === `${settings.homepage}`) {
+              document.body.classList.add('homepage');
+            } else {
+              document.body.classList.remove('homepage');
+            }
+          case 'top-menu':
+            const topMenu = this.siteSettings.top_menu;
+            const targets = topMenu.split('|').map((opt) => `discovery.${opt}`);
+            if (targets.includes(currentRoute)) {
+              document.body.classList.add('homepage');
+            } else {
+              document.body.classList.remove('homepage');
+            }
         }
       });
     });
